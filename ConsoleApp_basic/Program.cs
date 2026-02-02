dee8dec247e1d65ad101e6105e926bd6a38c9f2e
Robert-J-Wang <robertwang6486@gmail.com>
2026-02-02 17:00:44 +1300

    hello world

---
+++ ConsoleApp_basic/Program.cs
@@ -0,0 +1,13 @@
+﻿// 命名空间：组织标识代码，防止代码冲突，类似理解哪个班级的张三？？
+namespace ConsoleApp_basic;
+
+// Program类， 所有代码都会放在内存中开辟的Program区域中执行
+class Program
+{
+    // 程序的入口方法，所以代码运行的逻辑都放在这个方法里执行
+    static void Main()
+    {
+        // 逻辑代码
+        Console.WriteLine("Hello, World!");
+    }
+}

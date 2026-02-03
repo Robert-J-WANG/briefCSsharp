a8e21ff41b2a8e12de8b05cb1bdda356a50c0913
Robert-J-Wang <robertwang6486@gmail.com>
2026-02-04 11:01:38 +1300

    class basic usage

--- ConsoleApp_basic/Program.cs
+++ ConsoleApp_basic/Program.cs
@@ -2,39 +2,28 @@
 
 class Program
 {
+    class Person
+    {
+        // 成员变量
+        public string _name;
+        public int _age;
+        // 静态变量
+        public static int maxAge=120;
+    }
     static void Main()
     {
-        double d = 1.234;
-        float f = 234.123f;
-        d = f;
-        // f = d; // double的范围更大。无法隐式转化为float
-        f = (float)d; // 显式强制转换
-
-        int i = 123;
-        short s = 456;
-        i = s;
-        // s = i; // int 的范围更大,无法隐式转化为short
-        s = (short)i; // 显式强制转换
-
-        bool connected = true;
-        bool unconnected = false;
-        int c = Convert.ToInt32(connected);
-        int u = Convert.ToInt32(unconnected);
-        Console.WriteLine($"c={c}, u={u}");
-
-        connected = Convert.ToBoolean(c);
-        unconnected = Convert.ToBoolean(u);
-        Console.WriteLine($"connected={connected}, unconnected={unconnected}");
-
-        int ii = 123456;
-        string int_str = ii.ToString();
-        int.TryParse(int_str, out int str_int);
-        Console.WriteLine($"int_str={int_str}, str_int={str_int}");
+        Person allice=new Person();
+        allice._name = "Allice";
+        allice._age = 20;
+        Console.WriteLine($"allice name is {allice._name}, age is {allice._age}");
         
-        bool flag = true;
-        string bool_str = flag.ToString();
-        bool.TryParse(bool_str, out bool str_bool);
-        Console.WriteLine($"bool_str={bool_str}, str_bool={str_bool}");
+        Person bob = new Person();
+        bob._name = "Bob";
+        bob._age = 30;
 
+        Console.WriteLine($"bob name is {bob._name}, age is {bob._age} ");
+        // 访问静态变量
+        Console.WriteLine($"the max age of person is {Person.maxAge}");
+        
     }
 }

7df0529349849b22b5f3e71e4916874f9e2c339e
Robert-J-Wang <robertwang6486@gmail.com>
2026-02-04 23:57:53 +1300

    value/reference types

--- ConsoleApp_basic/Program.cs
+++ ConsoleApp_basic/Program.cs
@@ -2,63 +2,49 @@
 
 class Program
 {
-    class MyMethod
+    class Person
     {
-        //成员（实例）方法
-        // public void Greet()
-        // {
-        //     Console.WriteLine("Hello World!");
-        // }
+        public string _name;
+        public int _age;
         
-        public void Greet()=> Console.WriteLine("Hello World!");
-       
-        //静态方法
-        // public static void Greet2()
-        // {
-        //     Console.WriteLine("Hello World too!");
-        // }
-        //
-        public static void Greet2() => Console.WriteLine("Hello World too!");
-       
-
-        /*
-         * 加密方法
-         */
-        // public static int Encrypt(int x)
-        // {
-        //     return x * 2 + 4;
-        // }
-        //
-        public static int Encrypt(int x)=>x * 2 + 4;
-
-        /*
-         * 解密方法
-         */
-        // public static int Decrypt(int x)
-        // {
-        //     return (x - 4) / 2;
-        // }
-        public static int Decrypt(int x)=>(x - 4) / 2;
-
+        // 构造器方法，通过传参的方式创建实例对象
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
     }
     
-    
+    static void SetAge(Person person, int age)=> person._age = age;
+    static void SetInt(int m, int  n) => m = n;
+
+    public static double Example(double d)
+    {
+        double e = d;
+        return e;
+    }
+
     static void Main()
     {
-        // 使用成员（实例）方法
-        MyMethod m = new MyMethod();
-        m.Greet();
+        var person = new Person("John", 22);
+        var person2 = person;
+        person._age = 33;
+        Console.WriteLine($"person is {person._name},  age is {person._age}");
+        Console.WriteLine($"person2 is {person2._name},  age is {person2._age}");
         
-        // 使用静态方法
-        MyMethod.Greet2();
         
-        // 加密一个数字
-        int number = 100;
-        int result=MyMethod.Encrypt(number);
-        Console.WriteLine($"加密后的数字是{result}");
-        
-        // 解密一个数字
-        int result2 = MyMethod.Decrypt(result);
-        Console.WriteLine($"解密后的数字是{result2}");
+        SetAge(person2,44);
+        Console.WriteLine($"person is {person._name},  age is {person._age}");
+        Console.WriteLine($"person2 is {person2._name},  age is {person2._age}");
+
+        int m = 100;
+        SetInt(m,0);
+        Console.WriteLine($"the value is {m}");
+
+        double result = Example(2.1415926);
+
+        string str = null; // 引用类型可以赋值为空
+        // int i = null; //值类型不能为空
+        int? i = null; // 加？设置为可空值
     }
 }

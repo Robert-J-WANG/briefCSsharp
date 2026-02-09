395347e95c47b39dc8f44049033f4892b636103f
Robert-J-Wang <robertwang6486@gmail.com>
2026-02-10 10:56:53 +1300

    exceptions

--- ConsoleApp_basic/Program.cs
+++ ConsoleApp_basic/Program.cs
@@ -4,33 +4,26 @@ class Program
 {
     static void Main()
     {
-        // int[] arr = new int[5];
-        // arr[0] = 1;
-        // arr[1] = 2;
-        // arr[2] = 3;
-        // arr[3] = 4;
-        // arr[4] = 5;
-        int[] arr = { 1, 2, 3, 4, 5 };
-        Console.WriteLine(arr[0]);
-
-        // 空数组
-        int[] arr2 = [];
-        Console.WriteLine(arr2.Length);
-        // arr2[0] = 1; // 长度固定，不能添加元素
-        // Console.WriteLine(arr2.Length); // 报错
-
-        //空list
-        var list = new List<int>();
-        Console.WriteLine(list.Count);
-        list.Add(1);
-        list.Add(2);
-        Console.WriteLine(list.Count);
-
-        int[,] grid =
+        try
         {
-            { 1, 2, 3, 4, 5 },
-            { 1, 2, 3, 4, 5 }
-        };
-        Console.WriteLine(grid[0, 0]);
+            int[] arr = { 1, 2, 3 };
+            Console.WriteLine(arr[3]);
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception");
+        }
     }
 }

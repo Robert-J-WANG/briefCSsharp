ba235a0d592b88ba355d3d3ce28692dda331106b
Robert-J-Wang <robertwang6486@gmail.com>
2026-02-09 15:35:58 +1300

    array

--- ConsoleApp_basic/Program.cs
+++ ConsoleApp_basic/Program.cs
@@ -2,25 +2,35 @@
 
 class Program
 {
-    class Person
-    {
-        public string _name;
-        public int _age;
-
-        public Person(string name, int age)
-        {
-            _name = name;
-            _age = age;
-        }
-    }
-
     static void Main()
     {
-        Person p=new Person("John", 23);
-        Person p2 = p;
-        p2._name = "Jerry";
+        // int[] arr = new int[5];
+        // arr[0] = 1;
+        // arr[1] = 2;
+        // arr[2] = 3;
+        // arr[3] = 4;
+        // arr[4] = 5;
+        int[] arr = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(arr[0]);
 
-        Console.WriteLine($"p name is {p._name},  age is {p._age}");
-        Console.WriteLine($"p2 name is {p2._name},   age is {p2._age}");
+        // 空数组
+        int[] arr2 = [];
+        Console.WriteLine(arr2.Length);
+        // arr2[0] = 1; // 长度固定，不能添加元素
+        // Console.WriteLine(arr2.Length); // 报错
+
+        //空list
+        var list = new List<int>();
+        Console.WriteLine(list.Count);
+        list.Add(1);
+        list.Add(2);
+        Console.WriteLine(list.Count);
+
+        int[,] grid =
+        {
+            { 1, 2, 3, 4, 5 },
+            { 1, 2, 3, 4, 5 }
+        };
+        Console.WriteLine(grid[0, 0]);
     }
 }

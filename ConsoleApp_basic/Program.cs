f909ac0cf3d58de0cc9b54aa87dc3553d3b85170
Robert-J-Wang <robertwang6486@gmail.com>
2026-02-10 14:05:42 +1300

    parameters and overload

--- ConsoleApp_basic/Program.cs
+++ ConsoleApp_basic/Program.cs
@@ -1,46 +1,67 @@
-﻿namespace ConsoleApp_basic;
+﻿using System.Globalization;
+
+namespace ConsoleApp_basic;
 
 class Program
 {
+    static int Add(int a) => a + 1;
+    static int Add(int a, int b) => a + b;
+    static double Add(int a, double b) =>a+b;
+    
+    
+    
+    // static void Add(int a, int b)=>Console.WriteLine(a + b);
+    // static int Add(int a, int b=1) => a + b;
+    
+    static void Print(int a)=> Console.WriteLine(a);
+
+    static void Change(ref int a, in int b)
+    {
+        a++;
+        // b++; // 报错，只读不能改
+        
+    }
+
+    static void UnChange(int a, int b)
+    {
+        a++;
+        b++;
+    }
+
+    static bool IsEqual(ref int num, out int copy)
+    {
+        copy = num;
+        num++;
+        return copy == num ? true : false;
+
+    }
+
+    static int AddMore(int a, params int [] nums)
+    {
+        int r = a;
+        foreach (var n in nums)
+        {
+            r += n;
+        }
+        return r;
+    }
+
     static void Main()
     {
-        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(Add(1, 2));
+        Console.WriteLine(Add(1));
+        Console.WriteLine(Add(b:2,a:3));
+        Console.WriteLine(AddMore(1, 2,3,4,5));
 
-        // // for loop
-        // for (int i = 0; i < numbers.Length; i++)
-        // {
-        //     Console.WriteLine(numbers[i]);
-        // }
-        //
-        // //foreach loop
-        // foreach (int number in numbers)
-        // {
-        //     Console.WriteLine(number);
-        // }
-        //
-        // // while loop
-        // int time = 0;
-        // while (time < numbers.Length)
-        // {
-        //     Console.WriteLine(numbers[time]);
-        //     time++;
-        // }
-        //
-        // //do while loop
-        // do
-        // {
-        //     Console.WriteLine("hello world");
-        //     time++;
-        // } while (numbers.Length > 0);
+        int a = 1;
+        int b = 2;
+        UnChange(a,b);
+        Console.WriteLine($"a:{a}, b:{b}");
+        Change(ref a, b);
+        Console.WriteLine($"a:{a}, b:{b}");
 
-        foreach (var number in numbers)
-        {
-            Console.WriteLine(number);
-            break;
-            // continue;
-            // return;
-        }
-
-        Console.WriteLine("hello world");
+        int num = -1;
+        bool result= IsEqual(ref num, out int copy);
+        Console.WriteLine($"result:{result}, num:{num}, copy:{copy}");
     }
 }
